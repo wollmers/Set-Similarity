fae7390d08d6d15052c3a6d5fbfd8e2dea0a79fd
Helmut Wollmersdorfer <helmut@wollmersdorfer.at>
2014-08-16 23:43:31 +0200

    0.017

--- lib/Set/Similarity.pm
+++ lib/Set/Similarity.pm
@@ -3,7 +3,7 @@ package Set::Similarity;
 use strict;
 use warnings;
 
-our $VERSION = '0.016';
+our $VERSION = '0.017';
 
 use Carp 'croak';
 
--- lib/Set/Similarity/Cosine.pm
+++ lib/Set/Similarity/Cosine.pm
@@ -5,7 +5,7 @@ use warnings;
 
 use parent 'Set::Similarity';
 
-our $VERSION = '0.016';
+our $VERSION = '0.017';
 
 sub from_sets {
   my ($self, $set1, $set2) = @_;
